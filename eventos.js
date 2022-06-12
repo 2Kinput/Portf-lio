1789191d92d3f677190c798d2b287b60fb7aebf5
Brunno_Devs <62145628+2Kinput@users.noreply.github.com>
2022-06-12 10:23:22 -0300

    commit inicial
    Primeiro commit do site

---
+++ eventos.js
@@ -0,0 +1,7 @@
+function entrar(){
+    window.open('http://127.0.0.1:5500/views.html')
+    }
+
+function whats(){
+    window.open('https://api.whatsapp.com/send?1=pt_BR&phone=5575992827557')
+}    
